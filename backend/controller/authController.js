b3426c518ee2ac739ee4ee48d55339c378a23e36
Preksha Yadav <ypreksha43@gmail.com>
2026-02-08 23:20:50 +0530

    backend changed

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -28,7 +28,7 @@ export const signUp = async (req,res) => {
             httpOnly:true,
             secure:false,
             sameSite:"Strict",
-            maxAge: 7 * 24 * 60 * 60 * 1000
+            maxAge: 1 * 24 * 60 * 60 * 1000
         })
         return res.status(201).json(user)
     } catch (error) {
@@ -52,7 +52,7 @@ export const login = async (req, res) => {
             httpOnly:true,
             secure:false,
             sameSite:"Strict",
-            maxAge: 7 * 24 * 60 * 60 * 1000
+            maxAge: 1 * 24 * 60 * 60 * 1000
         })
         return res.status(200).json(user)
     } catch (error) {
