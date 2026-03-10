53d28d1f5e9636a2dfd6cf1652811241eb379c9d
Preksha Yadav <ypreksha43@gmail.com>
2026-03-10 10:32:06 +0530

    Lectures APIs

--- backend/controller/courseController.js
+++ backend/controller/courseController.js
@@ -1,11 +1,13 @@
 import uploadOnCloudinary from "../config/cloudinary.js"
 import Course from "../model/courseModel.js"
+import Lecture from "../model/lectureModel.js"
 
 
 //create course
 export const createCourse = async (req,res) => {
     try {
         const {title, category} = req.body
+
         if(!title || !category){
             return res.status(400).json({
                 message:"Title and category are required"
@@ -23,8 +25,12 @@ export const createCourse = async (req,res) => {
     }
 }
 
+
+//Get all published courses
 export const getPublishedCourses = async (req,res) => {
     try {
+
+        // Find all courses where isPublished = true
         const courses = await Course.find({isPublished:true})
         if(!courses){
             return res.status(400).json({message:"No published courses found"})
@@ -35,9 +41,14 @@ export const getPublishedCourses = async (req,res) => {
     }
 }
 
+// GET COURSES CREATED BY CURRENT USER
 export const getCreatorCourses = async (req,res) => {
     try {
+
+        // Get logged-in user's ID
         const userId = req.userId
+
+        // Find courses where creator = logged in user
         const courses = await Course.find({creator:userId})
         if(!courses){
             return res.status(400).json({message:"No courses found for this creator"})
@@ -48,18 +59,27 @@ export const getCreatorCourses = async (req,res) => {
     }
 }
 
+// EDIT COURSE DETAILS
 export const editCourse = async (req,res) => {
     try {
+
+        // Get courseId from URL params
         const {courseId} = req.params
+        // Get updated course data from request body
         const {title,subTitle ,description, category, level,isPublished ,price} = req.body
         let thumbnail
+        // If thumbnail image is uploaded
         if(req.file){
             thumbnail = await uploadOnCloudinary(req.file.path)
         }
+
+        // Find course by ID
         let course = await Course.findById(courseId)
         if(!course){
             return res.status(400).json({message:"Course not found"})
         }
+
+        // Prepare updated data object
         const updateData =  {
                 title,
                 subTitle,
@@ -70,6 +90,7 @@ export const editCourse = async (req,res) => {
                 price,
                 thumbnail }
 
+                // Update course in database
             course = await Course.findByIdAndUpdate(courseId, updateData, {new:true})
         
         res.status(200).json({course:course})
@@ -78,6 +99,7 @@ export const editCourse = async (req,res) => {
     }
 }
 
+// GET COURSE BY ID
 export const getCourseById = async (req,res) => {
     try {
         const {courseId} = req.params
@@ -91,6 +113,7 @@ export const getCourseById = async (req,res) => {
     }
 }
 
+// DELETE COURSE
 export const removeCourse = async (req,res) => {
     try {
         const {courseId} = req.params
@@ -102,5 +125,110 @@ export const removeCourse = async (req,res) => {
         return res.status(200).json({message:"Course removed successfully"})
     } catch (error) {
         return res.status(500).json({message:`Error removing course ${error}`})
+    }
+}
+
+
+
+//LECTURE CONTROLLERS 
+
+// CREATE LECTURE
+export const createLecture = async (req,res) => {
+    try {
+        const {lectureTitle} = req.body
+        const {courseId} = req.params
+
+        // Validate lecture title
+        if(lectureTitle || courseId){
+            return res.status(400).json({message:"lectureTitle is required"})
+        }
+
+        // Create lecture document
+        const lecture = await Lecture.create({lectureTitle})
+        
+        // Find the course and add lecture to its lectures array
+        const course = await Course.findById({courseId})
+        if(!course){
+            course.lectures.push(lecture._id)
+        }
+
+        // Populate lectures field
+        await course.populate("lectures")
+        await course.save()
+
+        return res.status(201).json({lecture , course})
+    } catch (error) {
+        return res.status(500).json({message:`Error creating course ${error}`})
+    }
+}
+
+// GET LECTURES OF A COURSE
+export const getCourseLecture = async (req,res) => {
+    try {
+        const {courseId} = req.params
+        const course = await Course.findById(courseId)
+        if(!course){
+            return res.status(404).json({message:"Course not found"})
+        }
+
+        // Populate lectures array
+        await course.populate(lectures)
+        await course.save()
+
+        return res.status(201).json(course)
+    } catch (error) {
+        return res.status(500).json({message:`Error getting course lecture ${error}`})
+    }
+}
+
+
+// EDIT LECTURE
+export const editLecture = async (req,res)=>{
+    try {
+        const {lectureId} = req.params
+        const {isPreviewFree , lectureTitle} = req.body
+        const lecture = await Lecture.findById(lectureId)
+        if(!lecture){
+            return res.status(404).json({message:"Lecture not found"})
+        }
+        let videoUrl
+        // Upload video if file exists
+        if(req.file){
+            videoUrl = await uploadOnCloudinary(req.file.path)
+            lecture.videoUrl = videoUrl
+
+        }
+        // Update lecture title
+        if(lectureTitle){
+            lecture.lectureTitle = lectureTitle
+
+        }
+        // Update preview status
+        lecture.isPreviewFree = isPreviewFree
+
+        await lecture.save()
+        return res.status(200).json(lecture)
+    } catch (error) {
+        return res.status(500).json({message:`Error editting lecture ${error}`})
+        
+    }
+}
+
+// DELETE LECTURE
+export const removeLecture = async (req,res) =>{
+    try {
+        const {lectureId} = req.params
+        const lecture = await Lecture.findByIdAndDelete(lectureId)
+        if(!lecture){
+                return res.status(404).json({message:`Lecture not found ${error}`})
+
+        }
+        await Course.updateOne(
+            {lectures:lectureId},
+            {$pull:{lectures:lectureId}}
+        )
+        return res.status(200).json({message:"Lecture removed"})
+    } catch (error) {
+        return res.status(500).json({message:`Error removing lecture ${error}`})
     }
 }
--- backend/route/courseRoute.js
+++ backend/route/courseRoute.js
@@ -1,17 +1,24 @@
 import express from "express"
-import { createCourse, editCourse, getCourseById, getCreatorCourses, getPublishedCourses, removeCourse } from "../controller/courseController.js"
+import { createCourse, createLecture, editCourse, editLecture, getCourseById, getCourseLecture, getCreatorCourses, getPublishedCourses, removeCourse, removeLecture } from "../controller/courseController.js"
 import isAuth from "../middleware/isAuth.js"
 import upload from "../middleware/multer.js"
 
 
 const courseRouter = express.Router()
-
+//courses
 courseRouter.post("/create",isAuth, createCourse)
 courseRouter.get("/getpublished",isAuth,getPublishedCourses)
 courseRouter.get("/getcreator", isAuth, getCreatorCourses)
-courseRouter.put("/editcourse/:courseId", isAuth, upload.single("thumbnail"), editCourse)
-courseRouter.get("/getcourse", isAuth, getCourseById)
-courseRouter.delete("/deletecourse/:courseId", isAuth, removeCourse)
+courseRouter.post("/editcourse/:courseId", isAuth, upload.single("thumbnail"), editCourse)
+courseRouter.get("/getcourse/:courseId", isAuth, getCourseById)
+courseRouter.delete("/remove/:courseId", isAuth, removeCourse)
+
+//lecture
+
+courseRouter.post("/createlecture/:courseId", isAuth, createLecture)
+courseRouter.get("/courselecture/:courseId", isAuth, getCourseLecture)
+courseRouter.post("/editlecture/:lectureId", isAuth, upload.single("video"), editLecture)
+courseRouter.delete("/removelecture/:lectureId", isAuth, removeLecture)
 
 
 export default courseRouter
